2c18ca7e16b0950dfae798fa9770fb01c7894a57
connorosburn <cogeneral92@gmail.com>
2020-05-04 23:00:54 -0400

    adds a build script

--- scripts.sh
+++ scripts.sh
@@ -2,15 +2,23 @@ function buildtests() {
   files=""
   for d in `find chess/lib/GameEngine -type d`
   do
-    files="$files $(buildcpp $d)"
+    files="$files $(collectcpp $d)"
   done
   for d in `find chess/test -type d`
   do
-    files="$files $(buildcpp $d)"
+    files="$files $(collectcpp $d)"
   done
   main="lib/main.cpp"
   files=${files//$main/}
   g++ -std=c++17 $files -o tests
+}
+
+function collectcpp() {
+  files=""
+  for c in $1/*.cpp; do
+    files="$files $c"
+  done
+  echo $files
 }
 
 alias runtests='./tests'
